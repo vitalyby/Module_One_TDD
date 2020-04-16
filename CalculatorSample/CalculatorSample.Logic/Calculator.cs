d00f31c0ee6a582799a94065981970199ffe2d08
vitalyby <vitaly.by@gmail.com>
2020-04-16 21:00:14 +0300

    First commit

---
+++ CalculatorSample/CalculatorSample.Logic/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CalculatorSample.Logic
+{
+    public class Calculator
+    {
+        public ILogger _logger;
+
+        public Calculator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int Add(int x, int y)
+        {
+            _logger.Log($"Add {x} + {y}");
+            return x + y;
+        }
+
+        public int Minus(int x, int y)
+        {
+            _logger.Log($"Minus {x} - {y}");
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            _logger.Log($"Multiply {x} * {y}");
+            return x * y;
+        }
+
+        public double Divide(int x, int y)
+        {
+            _logger.Log($"Divide {x} / {y}");
+            return Math.Round((double) x / (double) y, 2);
+        }
+
+        public double Degree(int x, int y)
+        {
+            _logger.Log($"Degree {x} ^ {y}");
+            return Math.Round(Math.Pow(x, y), 3);
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.AppendAllText("d:\\cources\\asp.net\\tdd\\CalculatorSample\\logger.txt", message);
+            Debug.WriteLine("Debagggggggggg " + message);
+            Console.WriteLine("Debagg " + message);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
---
+++ CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using CalculatorSample.Logic;
+using Moq;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace CalculatorSample.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void TestAddPositive()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Add(5, 3);
+            mock.Verify(l => l.Log(It.IsAny<string>()),Times.Exactly(1));
+            Assert.AreEqual(expected:8,actual:actual);
+        }
+        [Test]
+        public void TestAddNegative()
+        {
+            var calc = new Calculator(new Logger());
+            var actual = calc.Add(-5, -4);
+            Assert.AreEqual(expected: -9, actual: actual);
+        }
+    }
+}

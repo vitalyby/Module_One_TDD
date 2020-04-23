51f81986d084d84e44abe4e03fb36cf83d08092a
vitalyby <vitaly.by@gmail.com>
2020-04-23 15:34:58 +0300

    + SearchNumbers

--- CalculatorSample/CalculatorSample.Logic/Calculator.cs
+++ CalculatorSample/CalculatorSample.Logic/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Security.Principal;
 
 namespace CalculatorSample.Logic
 {
@@ -34,6 +35,68 @@ namespace CalculatorSample.Logic
         {
             _logger.Log($"Log Divide: {x} / {y}");
             return Math.Round((double)x / (double)y, 2);
+        }
+
+        public int[] SearchNumbers(int x, int y)
+        {
+            _logger.Log($"Log SearchNumbers: {x} and {y}");
+            var result = new int[] { };
+
+            //проверка на отрицательные входные параметры
+            if (x < 0 || y < 0)
+            {
+                throw new ArgumentOutOfRangeException("Param < 0", new Exception());
+            }
+
+            //если входные параметры с нулями, отдалим пустой массив
+            if (x == 0 || y == 0)
+            {
+                return result;
+            }
+
+            var cnt = 0;
+            var minValue = 0;
+            int maxValue = 0;
+
+            for (int i = 1; i < Convert.ToInt32("9".PadRight(y, '9')); i++)
+            {
+                int sumValues = 0;
+                int lengthValue = i.ToString().Length;
+
+                //только для чисел, длина которых равна у
+                if (lengthValue == y)
+                {
+                    for (int ii = 0; ii < lengthValue; ii++)
+                    {
+                        var currValue = Convert.ToInt32(i.ToString().Substring(ii, 1));
+                        sumValues = sumValues + currValue;
+
+                    }
+                }
+
+                //если сумма соответствует входному параметру х
+                if (sumValues == x)
+                {
+                    cnt++;
+                    if (minValue == 0)
+                    {
+                        minValue = i;
+                    }
+
+                    if (i > maxValue)
+                    {
+                        maxValue = i;
+                    }
+                }
+            }
+
+            //если ничего не найдено пойдет пустой массив, иначе заполняем
+            if (cnt > 0)
+            {
+                result = new int[] { cnt, minValue, maxValue == 0 ? minValue : maxValue };
+            }
+
+            return result;
         }
     }
 
--- CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
+++ CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
@@ -1,6 +1,7 @@
 ﻿using CalculatorSample.Logic;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Diagnostics;
 
 namespace CalculatorSample.Tests
@@ -74,6 +75,56 @@ namespace CalculatorSample.Tests
             var calc = Init();
             var actual = calc.Divide(-6, -3);
             Assert.AreEqual(expected: 2, actual: actual);
+        }
+
+        //SearchNumbers
+
+        [Test]
+        public void TestSearchNumbersPositive()
+        {
+            var calc = Init();
+            var actual = calc.SearchNumbers(4, 2);
+            Assert.AreEqual(expected: new int[] { 4, 13, 40 }, actual: actual);
+        }
+
+        [Test]
+        public void TestSearchNumbersPositive2()
+        {
+            var calc = Init();
+            var actual = calc.SearchNumbers(12, 3);
+            Assert.AreEqual(expected: new int[] { 66, 129, 930 }, actual: actual);
+        }
+
+        [Test]
+        public void TestSearchNumbersPositive3()
+        {
+            var calc = Init();
+            var actual = calc.SearchNumbers(6, 1);
+            Assert.AreEqual(expected: new int[] { 1, 6, 6 }, actual: actual);
+        }
+
+        [Test]
+        public void TestSearchNumbersPositive4()
+        {
+            var calc = Init();
+            var actual = calc.SearchNumbers(12, 1);
+            Assert.AreEqual(expected: new int[]{}, actual: actual);
+        }
+
+        [Test]
+        public void TestSearchNumbersNegativeNumber()
+        {
+            var calc = Init();
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => calc.SearchNumbers(-4, 2));
+            Assert.IsTrue(ex.Message.Contains("Param < 0"));
+        }
+
+        [Test]
+        public void TestSearchNumbersNegativeNumber2()
+        {
+            var calc = Init();
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => calc.SearchNumbers(4, -2));
+            Assert.IsTrue(ex.Message.Contains("Param < 0"));
         }
     }
 }
